eadf11f866b298d302f043a37a27c78578f4dafc
bakmie234 <bakmie234@gmail.com>
2016-04-05 00:46:19 +0700

    init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+    public class Name
+    {
+        public string OurProperty { get; set; }
+    }
+}
